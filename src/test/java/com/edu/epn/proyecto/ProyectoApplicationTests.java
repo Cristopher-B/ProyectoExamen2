78060ec0de1f5471d50e58af4d80830670ddf786
Cristopher-B <cristopher.balseca@epn.edu.ec>
2026-01-18 15:25:56 -0500

    Spring Initializr

---
+++ src/test/java/com/edu/epn/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.epn.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
