801eb1a80108e627be07c1a92081d84631f756ac
Cristopher-B <cristopher.balseca@epn.edu.ec>
2026-01-25 17:46:10 -0500

    Actualizar archivos

---
+++ build/resources/main/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO estudiante (cedula, nombre, apellido, email, carrera, edad)
+VALUES ('1725489630', 'Juan', 'Pérez', 'juan.perez@example.com', 'Desarrollo de software', 20);
+
+INSERT INTO estudiante (cedula, nombre, apellido, email, carrera, edad)
+VALUES ('0912457863', 'María', 'López', 'm.lopez@instituto.edu', 'Administración', 22);
+
+INSERT INTO estudiante (cedula, nombre, apellido, email, carrera, edad)
+VALUES ('1805641239', 'Carlos', 'Vera', 'carlos.m@marketing.com', 'Marketing', 25);
+
+INSERT INTO estudiante (cedula, nombre, apellido, email, carrera, edad)
+VALUES ('1004587125', 'Lucía', 'Torres', 'ana.torres@salud.com', 'Farmacia', 19);
+
+INSERT INTO estudiante (cedula, nombre, apellido, email, carrera, edad)
+VALUES ('1758963214', 'Ricardo', 'Peña', 'rsuarez@contabilidad.ec', 'Contabilidad', 30);
